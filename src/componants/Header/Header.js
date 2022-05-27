36c8ba598f363fec9774477e564639c71db1ab4d
Asaduzzaman Alamin <asaduzzamanrossow2000@gmail.com>
2022-05-28 02:02:56 +0600

    customLink-end

--- src/componants/Header/Header.js
+++ src/componants/Header/Header.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import CustomLink from '../../CustomLink';
+
 import './Header.css';
 
 const Header = () => {
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
+            <Navbar  sticky="top" bg="dark" variant="dark">
     <Container>
     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
     <Nav className="ms-auto">
-      <Link to='/'>Home</Link>
-      <Link to='/countries'>Countries</Link>
-      <Link to='/about'>About Us</Link>
-      <Link to='/blog'>Blog</Link>
+      <div className='link-style'>
+     
+      
+      <CustomLink className='list' to='/'>Home</CustomLink>
+      <CustomLink className='list' to='/countries'>Countries</CustomLink>
+      <CustomLink className='list' to='/blog'>Blog</CustomLink>
+      <CustomLink className='list'  to='/about'>About Us</CustomLink>
+      
+      
+      </div>
     </Nav>
     </Container>
   </Navbar>
